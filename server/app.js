8abdbd9588a9e8bac639e7b5035d9436a06ce7e0
Erdem Kahraman <erdem.kahraman@tebilisim.com>
2025-10-29 13:01:12 +0300

    Son

---
+++ server/app.js
@@ -0,0 +1,25 @@
+import express from 'express'
+import cors from 'cors'
+import dotenv from 'dotenv'
+import cloudinaryRoutes from './routes/cloudinary.js'
+import { configureCloudinary } from './utils/cloudinaryUtils.js'
+
+dotenv.config()
+
+const app = express()
+
+configureCloudinary({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+})
+
+app.use(cors())
+app.use(express.json({ limit: '100mb' }))
+app.use(express.urlencoded({ limit: '100mb', extended: true }))
+
+app.use('/api', cloudinaryRoutes)
+
+app.get('/health', (_req, res) => res.json({ ok: true }))
+
+export default app
